8bf023257885d025d679726f462355ccb6d88585
dcollier <dcollier@uwaterloo.ca>
2013-04-30 16:01:25 -0400

    Created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitdatafeed::Application.config.secret_token = '3f5ad83dfdef4d69fd35fce7a94499e43c39306aab227218608b157fa6866634cac3843801bf178cc5ed9a6fea91cecc6948ff8b80567b8c9dc1f5ea479cd484'
